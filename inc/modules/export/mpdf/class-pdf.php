0f8a3227e23ff4a0a771a64f77d6626de547bcc3
bdolor <bpayne@bccampus.ca>
2017-12-15 12:44:57 -0800

    removing extraneous pagebreaks from difference in @page support

--- inc/modules/export/mpdf/class-pdf.php
+++ inc/modules/export/mpdf/class-pdf.php
@@ -35,7 +35,6 @@ namespace BCcampus\Modules\Export\Mpdf;
 use Mpdf\Mpdf;
 use Mpdf\MpdfException;
 use Masterminds\HTML5;
-use phpDocumentor\Reflection\Types\Boolean;
 use Pressbooks\Book;
 use Pressbooks\Sanitize;
 use Pressbooks\Modules\Export\Prince;
@@ -90,13 +89,6 @@ class Pdf extends Prince\Pdf {
 	 * @var array
 	 */
 	protected $bookMeta;
-
-	/**
-	 * Parses the html as styles and stylesheets only
-	 * @see http://mpdf1.com/manual/index.php?tid=121
-	 *
-	 */
-	const MODE_CSS = 1;
 
 	/**
 	 * endpoint for xhtml page
@@ -159,11 +151,7 @@ class Pdf extends Prince\Pdf {
 			$this->mpdf = new Mpdf( $config );
 			$this->setDocumentMeta();
 
-			// moar configuration
-			$this->mpdf->ignore_invalid_utf8 = true;
-			$this->mpdf->defaultfooterline   = 0;
-			$this->mpdf->defaultheaderline   = 0;
-			$this->mpdf->SetAnchor2Bookmark( 1 );
+			// @see https://mpdf.github.io/reference/mpdf-functions/overview.html
 			$this->mpdf->SetBasePath( home_url( '/' ) );
 			$this->mpdf->SetCompression( true );
 
@@ -175,7 +163,9 @@ class Pdf extends Prince\Pdf {
 			 * dumping the whole xhtml document in and using prince css
 			 * works but MPDF TOC and Bookmarks don't build
 			 * works but no page numbering
-			 * works but is more memory intensive for mpdf
+			 * works but is more memory intensive for mpdf (error msg below)
+			 * * The HTML code size is larger than pcre.backtrack_limit 1000000.
+			 * * You should use WriteHTML() with smaller string lengths
 			 * works but mpdf headers and footer functionality is lost
 			 *****************************************/
 			//$this->mpdf->WriteHTML( $contents );
@@ -193,7 +183,7 @@ class Pdf extends Prince\Pdf {
 	/**
 	 * Give Mpdf all the things
 	 * @see https://github.com/mpdf/mpdf/blob/development/src/Config/ConfigVariables.php
-	 * @see https://mpdf.github.io/reference/mpdf-variables/overview.html
+	 *
 	 */
 	private function setConfigVariables() {
 		// CSS File
@@ -210,24 +200,31 @@ class Pdf extends Prince\Pdf {
 
 		];
 
+		// cherry picked
 		$config = [
-			'mode'              => 's',
-			'format'            => 'Letter',
-			'default_font_size' => 0,
-			'default_font'      => '',
-			'margin_left'       => 15,
-			'margin_right'      => 15,
-			'margin_top'        => 16,
-			'margin_bottom'     => 16,
-			'margin_header'     => 9,
-			'margin_footer'     => 9,
-			'orientation'       => 'P',
-			'enableImports'     => false,
-			'anchor2Bookmark'   => 1,
-			'mirrorMargins'     => 1,
-			'tempDir'           => _MPDF_TEMP_PATH,
-			'defaultCssFile'    => $css_file,
-//				'fontDir' => _MPDF_TTFONTDATAPATH,
+			'mode'                 => 's',
+			'format'               => 'Letter',
+			'default_font_size'    => 0,
+			'default_font'         => '',
+			'margin_left'          => 15,
+			'margin_right'         => 15,
+			'margin_top'           => 16,
+			'margin_bottom'        => 16,
+			'margin_header'        => 9,
+			'margin_footer'        => 9,
+			'orientation'          => 'P',
+			'enableImports'        => false,
+			'anchor2Bookmark'      => 1,
+			'mirrorMargins'        => 1,
+			'tempDir'              => _MPDF_TEMP_PATH,
+			'defaultCssFile'       => $css_file,
+			'autoLangToFont'       => true,
+			'ignore_invalid_utf8'  => true,
+			'defaultfooterline'    => 0,
+			'defaultheaderline'    => 0,
+			'shrink_tables_to_fit' => 1,
+			'use_kwt'              => true,
+//			'debug'                => true,
 		];
 
 		// user config overrides defaults
@@ -244,6 +241,7 @@ class Pdf extends Prince\Pdf {
 	/**
 	 * Sets Available PDF Document Metadata
 	 * @see https://mpdf.github.io/setting-pdf-file-properties/document-metadata.html
+	 *
 	 */
 	protected function setDocumentMeta() {
 		( isset( $this->bookMeta['pb_title'] ) ) ? $this->mpdf->SetTitle( $this->bookMeta['pb_title'] ) : '';
@@ -257,8 +255,8 @@ class Pdf extends Prince\Pdf {
 	/**
 	 * Add the mpdf Table of Contents.
 	 * Note, the functionality of the TOC is limited: its behavior varies
-	 * according mirrored margin settings, and will always generate blank pages
-	 * after.
+	 * according mirrored margin settings, and will always start on an odd page and
+	 * is likely to generate unwanted blank pages after.
 	 * http://mpdf1.com/forum/discussion/comment/6417#Comment_6417
 	 *
 	 */
@@ -269,7 +267,7 @@ class Pdf extends Prince\Pdf {
 			'links'            => true,
 			'tocindent'        => 1,
 			'suppress'         => 'on',
-			'toc_bookmarkText' => 'toc',
+			'toc_bookmarkText' => 'Contents',
 			'toc_preHTML'      => '<h1 class="toc">Contents</h1>',
 		];
 
@@ -392,7 +390,6 @@ class Pdf extends Prince\Pdf {
 						$add_to_toc     = false;
 
 				}
-
 
 				/****************************************
 				 * Add Page to Document Array
@@ -591,11 +588,45 @@ class Pdf extends Prince\Pdf {
 
 	private function filterCss( $css ) {
 		$filtered = '';
+
+		// primary purpose is to deal with how mpdf class handles @page
 		if ( ! empty( $css ) ) {
-			$filtered = preg_replace( '/page-break-(before|after)\:(\s?)(right|auto|left|always|inherit);/i', '', $css );
+			// page breaks in mpdf are created with 'AddPage()'
+			$filtered = preg_replace( '/page-break-(before|after|inside)\:(\s?)(right|auto|left|always|inherit);/i', 'page-break-$1: avoid;', $css );
+
+			// mpdf has its own class for toc
+			$filtered = preg_replace( '/#toc\s?/iU', '.mpdf_toc ', $filtered );
+
+			// reference to page selectors force unwanted page breaks
+			$filtered = preg_replace( '/page\:\s?(.*);/iU', '', $filtered );
+
+			// page breaks created with every change in @page selector
+			$filtered = preg_replace( '/@page\s?(.*)\s{/iU', 'body {', $filtered );
+
+			// Mpdf has limited @page support
+			$filtered = preg_replace( '/(?:@(top*|bottom*|right*|left*|footnotes)\s?(.*)\s?{(.*)})/isU', '', $filtered );
+
 		}
 
-		return $filtered;
+		$mpdf_css = '
+div.mpdf_toc_level_0 { 
+	line-height: 1.5;
+	margin-left: 0;
+	padding-right: 0em;
+}
+div.mpdf_toc_level_1 { 
+	margin-left: 2em;
+	text-indent: -2em;
+	padding-right: 0em;
+}
+div.mpdf_toc_level_2 { 
+	margin-left: 4em;
+	text-indent: -2em;
+	padding-right: 0em; 
+}';
+
+		return $mpdf_css . $filtered;
+
 	}
 
 
