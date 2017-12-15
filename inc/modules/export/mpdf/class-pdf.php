e25db0c19ded58b25114f42de0bb3e4ff1be172e
bdolor <bpayne@bccampus.ca>
2017-12-15 12:51:33 -0800

    minifying toc mpdf css

--- inc/modules/export/mpdf/class-pdf.php
+++ inc/modules/export/mpdf/class-pdf.php
@@ -608,22 +608,7 @@ class Pdf extends Prince\Pdf {
 
 		}
 
-		$mpdf_css = '
-div.mpdf_toc_level_0 { 
-	line-height: 1.5;
-	margin-left: 0;
-	padding-right: 0em;
-}
-div.mpdf_toc_level_1 { 
-	margin-left: 2em;
-	text-indent: -2em;
-	padding-right: 0em;
-}
-div.mpdf_toc_level_2 { 
-	margin-left: 4em;
-	text-indent: -2em;
-	padding-right: 0em; 
-}';
+		$mpdf_css = 'div.mpdf_toc_level_0{line-height:1.5;margin-left:0;padding-right:0}div.mpdf_toc_level_1{margin-left:2em;text-indent:-2em;padding-right:0}div.mpdf_toc_level_2{margin-left:4em;text-indent:-2em;padding-right:0}';
 
 		return $mpdf_css . $filtered;
 
